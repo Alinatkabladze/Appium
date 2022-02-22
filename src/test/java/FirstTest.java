ef39a1b68527c90e15b0c320d92eccfc2e6ba37c
ATkabladze <alinatkabladze@gmail.com>
2022-02-22 21:07:49 +0400

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,42 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+public class FirstTest {
+    public AndroidDriver<MobileElement> driver;
+    public WebDriverWait                wait;
+    //Elements By
+    By digit           = By.id("com.google.android.calculator:id/digit_5");
+
+    @BeforeMethod
+    public void setup() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Pixel_5_API_30");
+        caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("platformVersion", "10.0");
+        caps.setCapability("appPackage", "com.google.android.calculator");
+        caps.setCapability("appActivity", "com.android.calculator2.Calculator");
+        caps.setCapability("noReset", "false");
+        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        wait = new WebDriverWait(driver, 10);
+    }
+  @Test
+    public void basicTest() throws InterruptedException {
+      //  MobileElement digit5=driver.findElement(By.id("com.google.android.calculator:id/digit_5"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(digit)).click();
+
+    }
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }
+}
